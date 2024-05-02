fee3e3aa1e983d4896ed335f213d72fbe1bbd800
csoloff <cassidysoloff@icloud.com>
2024-05-02 10:14:15 -0700

    Finished first draft of the neg_flow.py script

--- neg_flow.py
+++ neg_flow.py
@@ -41,15 +41,16 @@ def reader(path, first_var):
         d['end_dt'] = pd.to_datetime('2024-1-1') + pd.to_timedelta(d['EndTimeSt'], unit='D') - pd.Timedelta(days=1)
     return d
 
-def glob_reader(file_key, first_var):
+def glob_reader(file_key, first_var, subfolder = './data/'):
     '''
     Reads groups of data files and merges them into one
 
     :param file_key: shared key in filenames
     :param first_var: the name of the first column label
+    :param subfolder: name of the subfolder containing the data
     :return: pandas DataFrame
     '''
-    paths = sorted(glob.glob('./data/*'+file_key+'*'))
+    paths = sorted(glob.glob(subfolder+'*'+file_key+'*'))
     d = []
     for i in range(0, len(paths)):
         d.append(reader(paths[i], first_var))
@@ -80,11 +81,22 @@ def find_neg_flow(d, t_name = 'untitled'):
 
     nf_d = pd.DataFrame(data={'Start':d['dt'].iloc[start_points].reset_index(drop=True), 'End':d['dt'].iloc[end_points].reset_index(drop=True), 'Start_index':start_points, 'End_index':end_points, 'Duration':pd.Series(durations)})
 
-    nf_d.to_csv('./tables/nf_' + t_name)
+    nf_d.to_csv('./tables/nf_' + t_name+'.csv')
 
     return nf_d
 
-def nf_plotter(sems, nf_d):
+def nf_plotter(sems, nf_d, param, dash, f_name = 'untitled'):
+    '''
+    Plots areas of negative flow and plots RH set point and measured values within DASH
+
+    :param sems: sems pandas DataFrame
+    :param nf_d: negative flow DataFrame
+    :param param: sample parameters DataFrame
+    :param dash: dash flow DataFrame
+    :param f_name: name to add to end of saved figure
+    :return: None
+    '''
+
     fig, axes = plt.subplots(4, sharex=True)
 
     axes[0].plot(sems['dt'], sems['UpSt_Samp'], c='black')
@@ -93,10 +105,48 @@ def nf_plotter(sems, nf_d):
             for j in range(0, len(nf_d)):
                     axes[i].axvspan(sems['dt'].loc[nf_d.loc[j]['Start_index']], sems['dt'].loc[nf_d.loc[j]['End_index']], color='red', alpha=0.2)
     
+    x,y = [],[]
+    for i in range(0, len(param)):
+            row = param.iloc[i]
+            x.extend([row['st_dt'], row['end_dt']])
+            y.extend([row['RH_Targ'], row['RH_Targ']])
+
+    axes[1].plot(x,y, c='black')
+    axes[1].scatter(x,y, c='r', s=.5, zorder=100)
+
+    axes[1].set_ylabel('RH_Targ')
+    axes[2].plot(dash['dt'], dash['HM_RH'], c='black')
+    axes[2].set_ylabel('HM_RH')
+    axes[3].plot(dash['dt'], dash['HO_RH'], c='black')
+    axes[3].set_ylabel('HO_RH')
+    axes[3].set_xlabel('MT Time')
+    axes[3].xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+    fig.tight_layout()
+    fig.savefig('./figures/neg_flow_'+f_name+'.png', dpi=300, bbox_inches='tight')
+
     plt.show()
 
-sems = glob_reader('SEMS', '#DOY.Frac')
 
-sems_nf = find_neg_flow(sems)
+def run_all_functions(subfolder='./data/', xprt_f_name = 'untitled'):
+    '''
+    Finds negative flows, saves a table of times with negative flows, and plots the negative flow along with RH in DASH
 
-nf_plotter(sems, sems_nf)
+    :param subfolder: specify the subfolder which contains the data (put all data of interest in same directory)
+    :param xprt_f_name: the str you would like to append to the table and figure saved
+    '''
+
+    sems = glob_reader('SEMS', '#DOY.Frac', subfolder=subfolder)
+
+    sems_nf = find_neg_flow(sems, t_name=xprt_f_name)
+
+    param = glob_reader('SAMP_PARAM', '#StartTimeSt', subfolder=subfolder)
+
+    dash = glob_reader('DASH_FLOW', '#DOY.Frac', subfolder=subfolder)
+
+    nf_plotter(sems, sems_nf, param, dash, f_name=xprt_f_name)
+
+# Calls function to run all other negative flow functions
+# Put all the data including SEMS, SAMP_PARAM, and DASH_FLOW files in same directory and specify it in the function below after "subfolder="".
+# Specify the name you would like to add at the end of the negative flow table and figure which will be saved
+
+run_all_functions(subfolder='./data/', xprt_f_name = 'demo')
